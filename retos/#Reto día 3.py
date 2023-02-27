6d05d234d174f8fcda3a1a84a2285d3914b27a52
Jaiquisic23 <jaimequinterosicre@gmail.com>
2023-02-27 16:37:36 +0100

    Reto dia 3 arreglado

--- retos/#Reto día 3.py
+++ retos/#Reto día 3.py
@@ -1,19 +1,45 @@
-def play_tennis(points):
-    scores = {"P1": 0, "P2": 0}
-    score_names = {0: "Love", 1: "15", 2: "30", 3: "40"}
-    for point in points:
-        scores[point] += 1
-        if scores["P1"] >= 4 and scores["P1"] - scores["P2"] >= 2:
-            print("Ha ganado el P1")
-            return
-        elif scores["P2"] >= 4 and scores["P2"] - scores["P1"] >= 2:
-            print("Ha ganado el P2")
-            return
-        elif scores["P1"] >= 4 and scores["P2"] >= 4:
-            print("Deuce")
-            continue
-        else:
-            print(score_names[scores["P1"]] + " - " + score_names[scores["P2"]])
+from enum import Enum
 
-points = ["P1", "P1", "P2", "P2", "P1", "P2", "P1", "P1"]
-play_tennis(points)
+class Player(Enum):
+    P1 = 1
+    P2 = 2
+
+def tenis_game(points: list):
+    
+    game = ["Love", "15", "30", "40"]
+    p1_points = 0
+    p2_points = 0
+    finished = False
+    error = False
+    
+    for player in points:
+        
+        error = finished
+        
+        p1_points += 1 if player == Player.P1 else 0
+        p2_points += 1 if player == Player.P2 else 0
+        
+        if p1_points >= 3 and p2_points >= 3:
+            if not finished and abs(p1_points - p2_points) <= 1:
+                print ("Deuce" if p1_points == p2_points else
+                   "Ventaja P1" if p1_points > p2_points else "Ventaja P2")
+            else:
+                finished = True
+        else:
+            if p1_points < 4 and p2_points < 4:
+                print (f"{game[p1_points]} - {game[p2_points]}")
+            else: 
+                finished = True
+            
+    print("Los puntos jugados no son correctos" if error or not finished else
+          "Ha ganado el P1" if p1_points > p2_points else "Ha ganado el P2")
+    
+tenis_game([Player.P1, Player.P1, Player.P2, Player.P2,
+            Player.P1, Player.P2, Player.P1, Player.P1])
+
+tenis_game([Player.P1, Player.P1, Player.P2, Player.P2,
+           Player.P1, Player.P2, Player.P1, Player.P1, Player.P2, Player.P1])
+
+tenis_game([Player.P1, Player.P1, Player.P1, Player.P1, Player.P1, Player.P1])
+
+tenis_game([Player.P1, Player.P1])
